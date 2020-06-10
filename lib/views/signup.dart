7f590157937d46433464ec3c14dabb6e3250dfc2
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-06-11 00:09:57 +0300

    view form signup

---
+++ lib/views/signup.dart
@@ -0,0 +1,93 @@
+import 'package:chatapp/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SingUp extends StatefulWidget {
+  @override
+  _SingUpState createState() => _SingUpState();
+}
+
+class _SingUpState extends State<SingUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height - 50,
+            alignment: Alignment.bottomCenter,
+            child:Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration('email'),
+                  ),
+                  TextField(
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration('password'),
+                  ),
+                  SizedBox(height:8,),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text("ForgotPassword", style: simpleTextStyle(),),
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            const Color(0xff007EF4),
+                            const Color(0xff2A75BC)
+                          ]
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Sign In",
+                      style: mediumTextStyle(),
+                    ),
+                  ),
+                  SizedBox(height:8,),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Sign In with Google", style: TextStyle(
+                        color: Colors.black38,
+                        fontSize:18
+                    ),
+                    ),
+                  ),
+                  SizedBox(height:8,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have account?", style: mediumTextStyle(),),
+                      Text("Register now", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          decoration: TextDecoration.underline
+                      ),)
+                    ],
+                  ),
+                  SizedBox(height:8,),
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
