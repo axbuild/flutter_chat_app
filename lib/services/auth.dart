ee2c5c39797d551eab922f6bd80a970b163863d6
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-07-01 23:57:15 +0300

    google sign in

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,12 +1,15 @@
 import 'package:chatapp/modal/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthMethods {
  final FirebaseAuth _auth = FirebaseAuth.instance;
+ final GoogleSignIn _googleSignIn = GoogleSignIn();
 
  User _userFromFirebaseUser(FirebaseUser user) {
    return user != null ? User(userId: user.uid) : null;
  }
+
  Future signInWithEmailAndPassword(String email, String password) async {
    try{
      AuthResult result = await _auth.signInWithEmailAndPassword
@@ -45,4 +48,36 @@ class AuthMethods {
      print(e.toString());
    }
  }
+
+ Future googleSignIn() async {
+   try{
+     final GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+     print('before');
+     final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+      print('one');
+     final AuthCredential credential = GoogleAuthProvider
+         .getCredential(idToken: googleSignInAuthentication.idToken,
+            accessToken: googleSignInAuthentication.accessToken);
+     print('two');
+     AuthResult result = await _auth.signInWithCredential(credential);
+     print(result.user);
+     FirebaseUser firebaseUser = result.user;
+     return _userFromFirebaseUser(firebaseUser);
+   }catch(e){
+     print('exception');
+     print(e.toString());
+   }
+ }
+
+ Future googleSignOut() async {
+   try {
+     return await _auth.signOut().then((val) {
+       _googleSignIn.signOut();
+     });
+   }catch(e){
+     print(e.toString());
+   }
+ }
+
 }
