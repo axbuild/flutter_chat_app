5287eca04ce11ba5cd2a5f825b84590a058ff113
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-07-08 23:41:20 +0300

    prepare service locator to main screen

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,7 +1,15 @@
+
+import 'package:chatapp/business_logic/view_models/main_screen_viewmodel.dart';
 import 'package:get_it/get_it.dart';
+import 'log/logger.dart';
+import 'log/logger_sentry.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
+  
+  serviceLocator.registerLazySingleton<Logger>(() => SentryLogger());
+
+  serviceLocator.registerFactory<MainScreenViewModel>(() => MainScreenViewModel());
 
 }
