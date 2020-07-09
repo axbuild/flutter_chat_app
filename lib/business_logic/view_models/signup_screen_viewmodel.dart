c5f5eaceb757defc0a37648d69c1b1fbda39bce8
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-07-09 23:42:11 +0300

    signin signup split bl

---
+++ lib/business_logic/view_models/signup_screen_viewmodel.dart
@@ -0,0 +1,53 @@
+import 'package:chatapp/business_logic/utils/constants.dart';
+import 'package:chatapp/business_logic/utils/helperfunctions.dart';
+import 'package:chatapp/services/auth.dart';
+import 'package:chatapp/services/database.dart';
+import 'package:chatapp/ui/screens/chat_rooms_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreenViewModel extends ChangeNotifier {
+
+
+  bool isLoading = false;
+
+  AuthMethods authMethods = new AuthMethods();
+  DatabaseMethods databaseMethods = new DatabaseMethods();
+//  HelperFunctions helperFunctions = new HelperFunctions();
+
+  final formKey = GlobalKey<FormState>();
+  TextEditingController userNameTextEditingController = new TextEditingController();
+  TextEditingController emailTextEditingController = new TextEditingController();
+  TextEditingController passwordTextEditingController = new TextEditingController();
+
+  void signMeUp(BuildContext context){
+    if(formKey.currentState.validate()) {
+
+      isLoading = true;
+
+      Map<String, String> userInfoMap = {
+        "name" : userNameTextEditingController.text,
+        "email" : emailTextEditingController.text,
+        "token" : Constants.token
+      };
+
+      HelperFunctions.saveUserEmailInSharedPreference(emailTextEditingController.text);
+      HelperFunctions.saveUserNameInSharedPreference(userNameTextEditingController.text);
+
+      authMethods.signUpwithEmailAndPassword( emailTextEditingController.text,
+          passwordTextEditingController.text
+      ).then((val){
+
+        databaseMethods.uploadUserInfo(userInfoMap);
+        HelperFunctions.saveUserLoggedInSharedPreference(true);
+        Navigator.pushReplacement(context, MaterialPageRoute(
+            builder: (context) => ChatRoom()
+        ));
+      });
+    }
+  }
+
+  void loadData(){
+    notifyListeners();
+  }
+}
--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,5 +1,7 @@
 
 import 'package:chatapp/business_logic/view_models/main_screen_viewmodel.dart';
+import 'package:chatapp/business_logic/view_models/signin_screen_viewmodel.dart';
+import 'package:chatapp/business_logic/view_models/signup_screen_viewmodel.dart';
 import 'package:get_it/get_it.dart';
 import 'log/logger.dart';
 import 'log/logger_sentry.dart';
@@ -11,5 +13,7 @@ void setupServiceLocator() {
   serviceLocator.registerLazySingleton<Logger>(() => SentryLogger());
 
   serviceLocator.registerFactory<MainScreenViewModel>(() => MainScreenViewModel());
+  serviceLocator.registerFactory<SignInScreenModelView>(() => SignInScreenModelView());
+  serviceLocator.registerFactory<SignUpScreenViewModel>(() => SignUpScreenViewModel());
 
 }
