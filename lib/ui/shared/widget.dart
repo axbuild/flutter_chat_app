933a0570a3a00e1b42dfd6aac3d2ea9be91596bb
Said Abdulakhatov <s.abdulakhatov@gmail.com>
2020-08-24 00:09:28 +0300

    rooms events

--- lib/ui/shared/widget.dart
+++ lib/ui/shared/widget.dart
@@ -3,6 +3,7 @@ import 'package:chatapp/business_logic/utils/authenticate.dart';
 import 'package:chatapp/business_logic/utils/local.dart';
 import 'package:chatapp/business_logic/utils/universal_variables.dart';
 import 'package:chatapp/business_logic/view_models/rooms_screen_viewmodel.dart';
+import 'package:chatapp/ui/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 Widget appBarMain(BuildContext context){
@@ -73,85 +74,51 @@ Drawer drawer(BuildContext context, RoomsScreenViewModel model){
     child: ListView(
       padding: EdgeInsets.zero,
       children: <Widget>[
+
         DrawerHeader(
-          child: Text("You: ${Local.user.name}", style: TextStyle(
-            color: Colors.white,
-            fontSize: 17
-          )),
-          decoration: BoxDecoration(
-            color: Color(UniversalVariables.primeColor),
-          ),
-        ),
-        ListTile(
-          title: Row(
-            children: <Widget>[
-              Text('Profile'),
-              Spacer(flex: 2),
-              Icon(
-                  Icons.keyboard_arrow_right,
-                  color: Colors.black,
-                  size: 28.0
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[
+                  Color(UniversalVariables.primeColor),
+                  Color(UniversalVariables.lightColor)
+                ])
+            ),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Material(
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    elevation: 10,
+                    child: Padding(padding: EdgeInsets.all(1.0),
+                      child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage:
+                        NetworkImage('https://avatars3.githubusercontent.com/u/13711097?s=460&u=1091476a60191df4ea63d5c7691e09830cf61df9&v=4'),
+                      ),
+                    ),
+                  ),
+                  Text(Local.user.name, style: TextStyle(color: Colors.white, fontSize: 25.0),)
+                ],
               ),
-            ],
-          ),
-          onTap: () {
-            // Update the state of the app.
-            // ...
-          },
-        ),
-        ListTile(
-          title: Row(
-            children: <Widget>[
-              Text('Balance'),
-              Spacer(flex: 2),
-              Icon(
-                  Icons.keyboard_arrow_right,
-                  color: Colors.black,
-                  size: 28.0
-              ),
-            ],
-          ),
-          onTap: () {
-            // Update the state of the app.
-            // ...
-          },
-        ),
-        ListTile(
-          title: Row(
-            children: <Widget>[
-              Text('Settings'),
-              Spacer(flex: 2),
-              Icon(
-                  Icons.keyboard_arrow_right,
-                  color: Colors.black,
-                  size: 28.0
-              ),
-            ],
-          ),
-          onTap: () {
-            // Update the state of the app.
-            // ...
-          },
-        ),
-        ListTile(
-          title: Row(
-            children: <Widget>[
-              Text('Sing out'),
-              Spacer(flex: 2),
-              Icon(
-                  Icons.keyboard_arrow_right,
-                  color: Colors.black,
-                  size: 28.0
-              ),
-            ],
-          ),
-          onTap: () {
-            model.signOut();
-            Navigator.pushReplacement(context, MaterialPageRoute(
-                builder: (context) => Authenticate()
-            ));
-          },
-        )
+            )),
+        CustomListTile(Icons.person, 'Profile', ()=>{
+          Navigator.pop(context),
+          Navigator.push(context,
+              new MaterialPageRoute(builder: (context) => new ProfileScreen())
+          )
+        }),
+        CustomListTile(Icons.calendar_today, 'Activity', ()=>{
+//          Navigator.pop(context),
+//          Navigator.push(context,
+//              new MaterialPageRoute(builder: (context) => new NotificationView())
+//          )
+        }),
+        CustomListTile(Icons.settings, 'Settings', ()=>{}),
+        CustomListTile(Icons.lock, 'Log Out', ()=>{
+          model.signOut(),
+          Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) => Authenticate()
+          ))
+        }),
       ],
     ),
   );
@@ -183,6 +150,49 @@ Future<void> showPopUpDialog(BuildContext context, String text) async {
       );
     },
   );
+}
+
+
+class CustomListTile extends StatelessWidget{
+
+  final IconData icon;
+  final  String text;
+  final Function onTap;
+
+  CustomListTile(this.icon, this.text, this.onTap);
+  @override
+  Widget build(BuildContext context){
+    //ToDO
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child:Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey.shade400))
+        ),
+        child: InkWell(
+            splashColor: Colors.orangeAccent,
+            onTap: onTap,
+            child: Container(
+                height: 40,
+                child: Row(
+                  mainAxisAlignment : MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(children: <Widget>[
+                      Icon(icon),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                      ),
+                      Text(text, style: TextStyle(
+                          fontSize: 16
+                      ),),
+                    ],),
+                    Icon(Icons.arrow_right)
+                  ],)
+            )
+        ),
+      ),
+    );
+  }
 }
 
 //Future<void> showLoadingDialog(BuildContext context, GlobalKey key) async {
